d3429c3a4900d508183ce36f5b09bd4ecaa95b33
Lucian Brănescu-Mihăilă <lucian1900@users.noreply.github.com>
2012-01-27 23:07:16 +0000

    Collision handling.

--- hashmap.py
+++ hashmap.py
@@ -24,7 +24,8 @@ class HashMap(Mapping):
 
     '''
     def __init__(self, mapping=[]):
-        self._length = 8
+        self._pow2 = 3
+        self._length = 2 ** self._pow2
         self._array = [None] * self._length
 
         self.update(mapping)
@@ -43,15 +44,48 @@ class HashMap(Mapping):
         for i in self._array:
             if i: mapping.append(i)
 
-    def _find(self, key):
-        return hash(key) % self._length
-
     def __getitem__(self, key):
-        key, value = self._array[self._find(key)]
-        return value
+        h = hash(key)
+        for i in xrange(self._length):
+            item = self._array[h % self._length]
+
+            if isinstance(item, tuple):
+                k, v = item
+                if k == key:
+                    return v
+
+            h >>= self._pow2
+            h += 1 # touch the entire array
+
+        raise KeyError("No such key: {}".format(key))
 
     def __setitem__(self, key, value):
-        self._array[self._find(key)] = (key, value)
+        h = hash(key)
+        for i in xrange(self._length):
+            index = h % self._length
+            item = self._array[index]
+
+            if not item:
+                self._array[index] = (key, value)
+                break
+
+            h >>= self._pow2
+            h += 1
+
+    def __delitem__(self, key):
+        h = hash(key)
+        for i in xrange(self._length):
+            index = h % self._length
+            item = self._array[index]
+
+            if isinstance(item, tuple):
+                k, v = item
+                if k == key:
+                    self._array[index] = True #dummy value
+                    return
+
+            h >>= self._pow2
+            h += 1
 
     def __iter__(self):
         return iter(self._array)
