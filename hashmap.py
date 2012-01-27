856964589f693284d8962dc1c62008a40e36b8c5
Lucian Brănescu-Mihăilă <lucian1900@users.noreply.github.com>
2012-01-27 00:46:35 +0000

    HashMap can be preloaded at construction. Size can be given at construction.

--- hashmap.py
+++ hashmap.py
@@ -5,18 +5,34 @@ class HashMap(object):
 
     >>> hm = HashMap()
     >>> hm[1] = 2
+    >>> hm._array
+    [0, (1, 2), 0, 0, 0, 0, 0, 0]
+    
     >>> hm[1]
     2
+
+    >>> hm['hello'] = 'world'
+    >>> hm._array
+    [0, (1, 2), 0, 0, 0, ('hello', 'world'), 0, 0]
+    
+    >>> h = HashMap([(1, 2), ('hello', 'world')])
+    >>> h._array
+    [0, (1, 2), 0, 0, 0, ('hello', 'world'), 0, 0]
+    
     '''
-    def __init__(self):
-        self._array = [0]*8
-        self._length = 8
+    def __init__(self, pairs=[], size=8):
+        self._length = size
+        self._array = [0] * self._length
+
+        for k, v in pairs:
+            self[k] = v
     
     def _find(self, key):
         return hash(key) % self._length
 
     def __getitem__(self, key):
-        return self._array[self._find(key)][1]
+        key, value = self._array[self._find(key)]
+        return value
 
     def __setitem__(self, key, value):
         self._array[self._find(key)] = (key, value)
