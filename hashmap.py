e5877bff65ba8f1fa0f0a02a23601288ed59abd3
Lucian Brănescu-Mihăilă <lucian1900@users.noreply.github.com>
2012-01-27 00:36:31 +0000

    Key/value tuples. Basic test passes. No collision checking or handling for now

--- hashmap.py
+++ hashmap.py
@@ -1,4 +1,13 @@
+#!/usr/bin/env python
+
 class HashMap(object):
+    '''
+
+    >>> hm = HashMap()
+    >>> hm[1] = 2
+    >>> hm[1]
+    2
+    '''
     def __init__(self):
         self._array = [0]*8
         self._length = 8
@@ -7,7 +16,11 @@ class HashMap(object):
         return hash(key) % self._length
 
     def __getitem__(self, key):
-        return self._array[self._find(key)]
+        return self._array[self._find(key)][1]
 
     def __setitem__(self, key, value):
-        self._array[self._find(key)] = value
+        self._array[self._find(key)] = (key, value)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
