91dd223ec07851904d296923b8fa872516bea8c0
Lucian Brănescu-Mihăilă <lucian1900@users.noreply.github.com>
2012-01-28 19:24:30 +0000

    Implement inplace quicksort. Test fails.

--- quicksort.py
+++ quicksort.py
@@ -9,21 +9,23 @@ arrays = [
 ]
 
 def quicksort(array):
-    '''
+    '''Naive recursive version. Middle is chosen as pivot
+
     >>> for i in arrays:
     ...     if quicksort(i) != sorted_array:
     ...         print False
+
     '''
 
     if len(array) <= 1:
         return array
 
-    mid = len(array) / 2
-    pivot = array[mid]
+    pivotIndex = len(array) / 2
+    pivot = array[pivotIndex]
 
     less = []
     more = []
-    for i in array[:mid] + array[mid+1:]:
+    for i in array[:pivotIndex] + array[pivotIndex + 1:]:
         if i < pivot:
             less.append(i)
         else:
@@ -31,8 +33,40 @@ def quicksort(array):
 
     return quicksort(less) + [pivot] + quicksort(more)
 
-def quicksort_inplace(array):
-    pass
+
+def swap(array, x, y):
+    array[y], array[x] = array[x], array[y]
+
+def partition(array, left, right, pivotIndex):
+    pivot = array[pivotIndex]
+
+    swap(array, pivotIndex, right) # move pivot to end
+    pivotIndex = left
+
+    for i in range(left, right):
+        if array[i] < pivot:
+            swap(array, i, pivotIndex)
+            pivotIndex += 1
+
+    swap(array, pivotIndex, right) # move pivot to final position
+
+    return pivotIndex
+
+def quicksort_inplace(array, left=0, right=None):
+    '''In place (no copies/concats). Uses partition function.
+
+    >>> for i in arrays:
+    ...     if quicksort_inplace(i) != sorted_array:
+    ...         print False
+    '''
+    if not right:
+        right = len(array) - 1
+
+    pivotIndex = (left + right) / 2
+    pivotIndex = partition(array, left, right, pivotIndex)
+
+    quicksort_inplace(array, left, pivotIndex - 1)
+    quicksort_inplace(array, pivotIndex + 1, right)
 
 if __name__ == '__main__':
     import doctest
