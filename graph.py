82dc2d7d1ba063a0695a4be99096a953b2c64f95
Lucian Brănescu-Mihăilă <lucian1900@users.noreply.github.com>
2012-01-29 17:45:46 +0000

    Dijkstra search.

--- graph.py
+++ graph.py
@@ -1,29 +1,50 @@
 #!/usr/bin/env python
 
-nodes_nested = {
-    1: {2: 2, 3: 5, 4: 1},
-    2: {1: 2, 6: 4},
-    3: {1: 5, 5: 1},
-    4: {1: 1, 5: 2, 7: 3, 8: 4},
-    5: {3: 1, 6: 2, 7: 8, 4: 2},
-    6: {2: 4, 5: 2, 9: 3},
-    7: {5: 8, 4: 3, 8: 1},
-    8: {4: 4, 7: 1, 9: 1},
-    9: {8: 1, 6: 3},
-}
+nodes = [
+    {1: 5},
+    {0: 5, 2: 2, 3: 5, 4: 1},
+    {1: 2, 6: 4},
+    {1: 5, 5: 1},
+    {1: 1, 5: 2, 7: 3, 8: 4},
+    {3: 1, 6: 2, 7: 8, 4: 2},
+    {2: 4, 5: 2, 9: 3},
+    {5: 8, 4: 3, 8: 1},
+    {4: 4, 7: 1, 9: 1},
+    {8: 1, 6: 3},
+]
 
-nodes = {
-    (1, 2): 2, (1, 3): 5, (1, 4): 1,
-    (2, 1): 2, (2, 6): 4,
-    (3, 1): 5, (3, 5): 1,
-    (4, 1): 1, (4, 5): 2, (4, 7): 3, (4, 8): 4,
-    (5, 3): 1, (5, 6): 2, (5, 7): 8, (5, 4): 2,
-    (6, 2): 4, (6, 5): 2, (6, 9): 3,
-    (7, 5): 9, (7, 4): 3, (7, 9): 1,
-    (8, 4): 4, (8, 7): 1, (8, 9): 1,
-    (9, 8): 1, (9, 6): 3,
-}
+def dijkstra(graph, start, end):
+    '''
 
+    >>> dijkstra(nodes, 1, 7)
+    (4, [1, 4, 7])
+    '''
+    path = [start]
+    costs = [float('inf')] * len(graph)
+    costs[start] = 0
+    seen = set([start])
+
+    node = start
+    while node != end:
+        edges = graph[node]
+
+        smallest = end
+        for i in edges.keys():
+            if i in seen:
+                continue
+
+            if edges[i] < costs[i]:
+                costs[i] = edges[i]
+                if edges[i] <= costs[smallest]:
+                    smallest = i
+
+        node = smallest
+        seen.add(node)
+        path.append(node)
+
+    print 'costs', costs, 'seen', seen
+
+    return costs[end], path
 
 if __name__ == '__main__':
     import doctest
