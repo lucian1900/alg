9e7d25bfcbfc0aac7d336e4b744d27c6b32fbb54
Lucian Brănescu-Mihăilă <lucian1900@users.noreply.github.com>
2012-02-01 11:05:12 +0000

    Add basic lcg formula.

--- rand.py
+++ rand.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python
 
+a = 4
+b = 2
+m = 7
+
+def lcg(n):
+    '''
+    >>> lcg(1)
+
+    >>> lcg(3)
+
+    >>> lcg(5)
+
+    '''
+    return (a * n + b) % 7
 
 if __name__ == '__main__':
     pass
