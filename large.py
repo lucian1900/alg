1a87de63a379b091e433c0cfb3435012d861c00a
Lucian Brănescu-Mihăilă <lucian1900@users.noreply.github.com>
2012-02-04 20:34:20 +0000

    Read and write files with mmap.

--- large.py
+++ large.py
@@ -1,20 +1,52 @@
 #!/usr/bin/env python
 
 import os
-from mmap import mmap
+from mmap import mmap, ACCESS_READ
 
 class Heap(object):
+    key_size = 3
+    count_size = 10
+    offset = key_size + count_size
+
     def __init__(self, filename):
         if not os.path.exists(filename):
-            f = open(filename, 'w')
+            f = open(filename, 'wb')
             f.write('\x00')
             f.close()
 
-        self.file = open(filename, 'r+')
+        self.file = open(filename, 'r+b')
         self.map = mmap(self.file.fileno(), 0)
 
-    def get(self, key):
-        pass
+    def incr(self, key):
+        size = self.map.size()
+
+        if size < size + self.offset:
+            self.map.resize(size + self.offset)
+
+        item = key.ljust(self.key_size) + self.pad(1)
+
+        self.map.write(item)
+
+    def pad(self, count):
+        return repr(count).zfill(self.count_size)
+
+    def __iter__(self):
+        i = 0
+        while i < self.map.size():
+            yield self.map[i:i+self.offset]
+            i += self.offset
+
+
+def process(filename='/home/lucian/Prog/alg/large.in'):
+    f = open(filename, 'r')
+    m = mmap(f.fileno(), 0, access=ACCESS_READ)
+    h = Heap('/home/lucian/Prog/alg/large.out')
+
+    while m.tell() < m.size():
+        line = m.readline()
+        key = line.split('\n')[0]
+
+        h.incr(key)
 
 
 def gendata(filename='/home/lucian/Prog/input'):
