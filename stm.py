42ac736f4855565c93dd4688b0ee3b13e29ac1e3
Lucian Brănescu-Mihăilă <lucian1900@users.noreply.github.com>
2012-02-05 19:20:54 +0000

    Merge STM object space into context manager.

--- stm.py
+++ stm.py
@@ -3,13 +3,13 @@
 from collections import Mapping
 from copy import deepcopy
 
-class Space(Mapping):
-    '''STM object space
 
-    >>> stm = Space()
+class Atomic(Mapping):
+    '''Transaction context manager. Implements STM semantics on top of
+    an object space (currently a dict). Might even work with locals()
 
-    >>> stm['hello'] = [1, 2, 3]
-    >>> stm['hello']
+    >>> stm = Atomic({})
+    >>> stm['hello'] = [1, 2, 3]; stm['hello']
     [1, 2, 3]
 
     >>> list(stm)
@@ -27,8 +27,15 @@ class Space(Mapping):
     >>> stm['hello']
     [1, 2, 3]
     '''
-    def __init__(self):
-        self.space = {}
+
+    def __init__(self, space):
+        self.space = space
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
 
     def __getitem__(self, key):
         val = self.space[key]
@@ -44,12 +51,16 @@ class Space(Mapping):
     def __iter__(self):
         return iter(self.space)
 
+    def __iter__(self):
+        return iter(self.space)
+
     def items(self):
         return list(self.iteritems())
 
     def iteritems(self):
         for key, val in self.space.iteritems():
             yield key, deepcopy(val)
+
 
 if __name__ == '__main__':
     import doctest
