5de415dc5f64d70f6405ba24384793ef30fe9e02
Lucian Brănescu-Mihăilă <lucian1900@users.noreply.github.com>
2012-02-05 23:01:38 +0000

    Move to attributes on space.

--- stm.py
+++ stm.py
@@ -3,73 +3,82 @@
 from time import time
 from copy import deepcopy
 from collections import Mapping
+from functools import wraps
 
 
 class RetryTransaction(Exception): pass
-class DictWrapper(dict): pass
 
 
 class atomic(object):
     '''Decorator for transactions
 
-    >>> s = Space({'a', 1})
+    >>> s = Space(a=1)
     >>> @atomic(s)
     ... def foo(space):
-    ...     space['a'] += 1
+    ...     space.a += 1
     >>> foo()
-    >>> s['a']
+    >>> s.a
+    2
+
+    >>> @atomic(s)
+    ... def bar(space):
+    ...     space.a += 2
+    ...     raise RetryTransaction()
+    >>> foo()
+    >>> s.a
     2
     '''
     def __init__(self, space):
         self.space = space
 
     def __call__(self, func):
+        @wraps(func)
         def wrap(*args, **kwargs):
-            result = func(self.space, *args, **kwargs)
+            self.space._start()
 
-            if self.space.write_time >= self.space.read_time:
-                print 'Failing transaction'
+            try:
+                result = func(self.space, *args, **kwargs)
+            except RetryTransaction:
+                self.space._commit(retry=True)
             else:
-                self.space.space.update(self.space.log)
-                self.space.write_time = time()
+                self.space._commit()
+                return result
 
         return wrap
 
+    def orElse(self, func):
+        pass
 
-class Space(Mapping):
+
+class Space(object):
     '''Transaction object space
 
     '''
 
-    def __init__(self, space):
-        self.space = space
-        self.log = {}
+    def __init__(self, **space):
+        self.__dict__['_store'] = space
+        self.__dict__['_log'] = {}
 
-        self.write_time = 0
-        self.read_time = time()
+        self.__dict__['_write_time'] = 0
+        self.__dict__['_read_time'] = time()
 
-    def __enter__(self):
-        self.read_time = time()
+    def _start(self):
+        self.__dict__['_read_time'] = time()
 
-    def __commit__(self):
-        if self.write_time >= self.read_time:
+    def _commit(self, retry=False):
+        if retry or \
+                self.__dict__['_write_time'] >= self.__dict__['_read_time']:
             print 'Failing transaction'
+            self.__dict__['_log'] = {}
         else:
-            self.space.update(self.log)
-            self.write_time = time()
+            self.__dict__['_store'].update(self.__dict__['_log'])
+            self.__dict__['_write_time'] = time()
 
-    def __getitem__(self, key):
-        return deepcopy(self.space[key])
+    def __getattr__(self, key):
+        return deepcopy(self.__dict__['_store'][key])
 
-    def __setitem__(self, key, val):
-        self.log[key] = val
-
-    def __len__(self):
-        return len(self.space)
-
-    def __iter__(self):
-        return iter(self.space)
-
+    def __setattr__(self, key, val):
+        self.__dict__['_log'][key] = val
 
 if __name__ == '__main__':
     import doctest
