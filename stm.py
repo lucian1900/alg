08655b164ae28dcb82e3a6f5739100b493770c0c
Lucian Brănescu-Mihăilă <lucian1900@users.noreply.github.com>
2012-02-05 18:57:18 +0000

    Space behaves as a dict with immutable values.

--- stm.py
+++ stm.py
@@ -1,6 +1,55 @@
 #!/usr/bin/env python
 
+from collections import Mapping
+from copy import deepcopy
 
+class Space(Mapping):
+    '''STM object space
+
+    >>> stm = Space()
+
+    >>> stm['hello'] = [1, 2, 3]
+    >>> stm['hello']
+    [1, 2, 3]
+
+    >>> list(stm)
+    ['hello']
+
+    >>> list(stm.items())
+    [('hello', [1, 2, 3])]
+
+    >>> l = stm['hello']; l
+    [1, 2, 3]
+
+    >>> l[0] = 5; l
+    [5, 2, 3]
+
+    >>> stm['hello']
+    [1, 2, 3]
+    '''
+    def __init__(self):
+        self.space = {}
+
+    def __getitem__(self, key):
+        val = self.space[key]
+        return deepcopy(val)
+
+    def __setitem__(self, key, val):
+        val = deepcopy(val)
+        self.space[key] = val
+
+    def __len__(self):
+        return len(self.space)
+
+    def __iter__(self):
+        return iter(self.space)
+
+    def items(self):
+        return list(self.iteritems())
+
+    def iteritems(self):
+        for key, val in self.space.iteritems():
+            yield key, deepcopy(val)
 
 if __name__ == '__main__':
     import doctest
