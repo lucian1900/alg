4d9f83d32e257cd496d92c7ceb870a0ccfc34a32
Lucian Brănescu-Mihăilă <lucian1900@users.noreply.github.com>
2012-02-14 13:36:40 +0000

    Add free.

--- gc.py
+++ gc.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 class Heap(object):
-    def __init__(self):
-        self._mem = []
+    def __init__(self, initial=None):
+        self._mem = initial or []
 
     def alloc(self, size):
         '''malloc equivalent
@@ -13,6 +13,16 @@ class Heap(object):
         chunk = [0] * size
         self._mem.extend(chunk)
         return chunk
+
+    def free(self, offset, length):
+        '''
+        >>> h = Heap([1, 0, 4, 5, 1, 2, 3]); h.free(3, 2); h._mem
+        [1, 0, 4, 0, 0, 2, 3]
+
+        >>> h = Heap([1, 2]); h.free(4, 1); h._mem
+
+        '''
+        self._mem[offset:offset+length] = [0] * length
 
 if __name__ == '__main__':
     import doctest
