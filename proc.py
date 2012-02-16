012d76239f8b21a0e6f46ffe0a38e7999530eac1
Lucian Brănescu-Mihăilă <lucian1900@users.noreply.github.com>
2012-02-16 20:54:17 +0000

    Add client-server manager setup. Client can't see manager callable.

--- proc.py
+++ proc.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
-import multiprocessing as mp
+import sys
+from multiprocessing import Pool
+from multiprocessing.managers import BaseManager
 
-def f(x):
-    return x * x
 
 if __name__ == '__main__':
-    p = mp.Pool(5)
-    print p.map(f, [1, 2, 3, 4, 5])
+    if sys.argv[1] == 'server':
+        l = [1, 2, 3]
+        manager = BaseManager(address=('', 50000), authkey='abc')
+        manager.register('get_l', callable=lambda: l)
+
+        server = manager.get_server()
+        server.serve_forever()
+
+    elif sys.argv[1] == 'client':
+        manager = BaseManager(address=('127.0.0.1', 50000), authkey='abc')
+        manager.connect()
+
+        print manager.get_l()
